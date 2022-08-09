aa45bbf3de918ce02934fc870814682c9aa80da8
jeho0131 <64771576+jeho0131@users.noreply.github.com>
2022-08-09 11:55:07 +0900

    Add files via upload

--- 2048JEHO/2048copy.py
+++ 2048JEHO/2048copy.py
@@ -1,12 +1,19 @@
-import pygame, sys
+import pygame, sys, random
 from pygame.locals import *
 
 pygame.init()
 
 GRAY = (224, 224, 224)
 DARKGRAY = (128, 128, 128)
+WHITE = (255,255,255)
+
 screen = pygame.display.set_mode((520, 520))
 array = [[0 for i in range(4)] for j in range(4)]
+spawn = [2,2,2,2,4]
+
+#numF = pygame.font.SysFont(None, 200)
+image2 = pygame.image.load("2.png").convert()
+image4 = pygame.image.load("4.png").convert()
 
 def DrawMap():
     screen.fill(GRAY)
@@ -15,8 +22,26 @@ def DrawMap():
         pygame.draw.line(screen, DARKGRAY, (i * 129, 0), (i * 129, 520), 8)
         pygame.draw.line(screen, DARKGRAY, (0, i * 129), (520, i * 129), 8)
 
-#def DrawNumber(n):
-    
+def DrawNumber(n,x,y):
+    #rect = pygame.Rect(129 * x + 6, 129 * y + 6, 120, 120)
+    #pygame.draw.rect(screen, WHITE, rect)
+    screen.blit(n, (129 * x + 6, 129 * y + 6))
+
+def SpawnNumber(n):
+    x = random.randint(0, 3)
+    y = random.randint(0, 3)
+
+    if array[y][x] != 0:
+        SpawnNumber(n)
+    else:
+        array[y][x] = n
+        DrawNumber(n, x, y)
+
+DrawMap()
+Inum = "{}{}".format("image", 2)
+print(Inum)
+SpawnNumber(Inum.strip(' " '))
+SpawnNumber(image2)
 
 while True:
     for event in pygame.event.get():
@@ -24,5 +49,4 @@ while True:
             pygame.quit()
             sys.exit()
 
-    DrawMap()  
     pygame.display.update()
